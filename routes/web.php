7e5fd742e13200b9d9a71cd7de8b5779c64f2a83
triastartya <triastartya@gmail.com>
2024-08-20 22:28:05 +0700

    'tenantcy'

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/user',function(){
-    return User::all();
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('health',function(){
+            return response()->json(['success'=>true,'data'=>'health']);
+        });
+        Route::get('/', function () {
+            dd(\App\Models\User::all());
+            return view('welcome');
+        });
+        // Route::get('tenant/create',[TenantController::class,'create']);
+        // your actual routes
+    });
+}
